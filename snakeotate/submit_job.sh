a5f105f304434f901ddbf6ceadf54d76591a98d1
bioinformaticsguy <bioinformaticsguy@gmail.com>
2025-11-26 15:04:25 +0100

    Update submit_job.sh to include commented Snakemake module load and clarify workflow execution steps

--- snakeotate/submit_job.sh
+++ snakeotate/submit_job.sh
@@ -14,6 +14,9 @@
 ## Usage check
 
 
+# ## Load Snakemake module
+# module load snakemake/9.13.7
+
 # set up path for conda env
 PATH=/work/hassan/hassan/miniforge:$PATH
 source /work/hassan/hassan/miniforge/etc/profile.d/conda.sh
@@ -28,14 +31,16 @@ snakemake --version
 # Print working directory
 echo "Working directory: $(pwd)"
 
-# # Run Snakemake workflow
-# snakemake -c 16 --use-conda --snakefile snakeotate/workflow/Snakefile --configfile snakeotate/resources/snv_annotation/real_config.yaml
+## Run Snakemake with small test data config (Works)
+# snakemake -c 16 --use-conda --snakefile snakeotate/workflow/Snakefile --configfile snakeotate/resources/snv_annotation/config.yaml
 
+# # Run Snakemake workflow with real data config
+# snakemake -c 32 --use-conda --snakefile snakeotate/workflow/Snakefile --configfile snakeotate/resources/snv_annotation/real_config.yaml
 
-# # Run Snakemake workflow
-snakemake -c 16 --use-conda --snakefile snakeotate/workflow/Snakefile --configfile snakeotate/resources/snv_annotation/config.yaml
+# # # Run Snakemake workflow with slurm profile
+snakemake -c 16 --use-conda --snakefile snakeotate/workflow/Snakefile --configfile snakeotate/resources/snv_annotation/config.yaml --profile snakeotate/myprofile
 
-
+# snakemake --profile snakeotate/myprofile --configfile snakeotate/resources/snv_annotation/config.yaml
 
 # # # Run Snakemake workflow
 # snakemake -c 16 --use-conda --snakefile snakeotate/workflow/Snakefile --configfile snakeotate/resources/snv_annotation/config.yaml --executor slurm  
